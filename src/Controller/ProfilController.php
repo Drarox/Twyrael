d0b9b69b71af463a90f897763ef24772a9fe999e
Yannick <yannick.burkard@etu.unsitra.fr>
2019-03-10 20:48:32 +0100

    Adding of block and reverse function

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -14,15 +14,25 @@ class ProfilController extends AbstractController
     /**
      * @Route("/profil/{id}", name="use Symfony\Component\Security\Core\User\UserInterface;profil")
      */
-    public function index($id)
+    public function index($id, UserInterface $user)
     {
+        $currentUser= $user->getId();
         $em = $this->getDoctrine()->getManager ();
         $repository = $em->getRepository ( Utilisateur::class);
         $result = $repository->getUsername($id);
 
+        $followrep = $em->getRepository ( Follow::class);
+        $followresult = $followrep->alreadyExist($currentUser,$id);
+        if ($followresult == null) { $alreadyFollow=false; } else { $alreadyFollow=true; }
+
+        $blockrep = $em->getRepository ( Bloque::class);
+        $blockresult = $blockrep->alreadyExist($currentUser,$id);
+        if ($blockresult == null) { $alreadyBlock=false; } else { $alreadyBlock=true; }
 
         return $this->render('profil/profil.html.twig', [
             'result' => $result,
+            'alreadyFollow' => $alreadyFollow,
+            'alreadyBlock' => $alreadyBlock,
         ]);
     }
 
@@ -63,6 +73,50 @@ class ProfilController extends AbstractController
         $entityManager->flush();
 
         echo "<script>alert('Compte bloqué');</script>";
+
+        return $this->redirectToRoute('home');
+    }
+
+    /**
+     * @Route("/unfollow/{id}", name="unfollow")
+     */
+    public function unfollow($id, UserInterface $user)
+    {
+        $currentUser= $user->getId();
+        $em = $this->getDoctrine()->getManager ();
+
+        $followrep = $em->getRepository ( Follow::class);
+        $followresult = $followrep->alreadyExist($currentUser,$id);
+
+        if ($followresult != null) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($followresult[0]);
+            $entityManager->flush();
+        }
+
+        echo "<script>alert('Ce compte n\'est plus suivi');</script>";
+
+        return $this->redirectToRoute('home');
+    }
+
+    /**
+     * @Route("/unblock/{id}", name="unfollow")
+     */
+    public function unblock($id, UserInterface $user)
+    {
+        $currentUser= $user->getId();
+        $em = $this->getDoctrine()->getManager ();
+
+        $blockrep = $em->getRepository ( Bloque::class);
+        $blockresult = $blockrep->alreadyExist($currentUser,$id);
+
+        if ($blockresult != null) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($blockresult[0]);
+            $entityManager->flush();
+        }
+
+        echo "<script>alert('Ce compte n\'est plus bloqué');</script>";
 
         return $this->redirectToRoute('home');
     }
